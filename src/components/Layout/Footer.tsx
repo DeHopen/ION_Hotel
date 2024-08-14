d9c84d384aa307e22cb77b71e0a54fd98e1a8343
DeHopen <a2302004b23@yandex.ru>
2024-08-14 15:23:54 +0300

    Fix bug with line in footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -6,13 +6,10 @@ const Footer = () => {
   return (
       <footer className={styles.footer}>
         <div className={styles.footerContainer}>
-          {/* Logo and Copyright */}
           <div className={styles.footerLogoText}>
-            <img src="/Logo/Logo_light.svg" alt="ION Hotel Logo" className={styles.logo} />
+            <img src="/Logo/Logo_light.svg" alt="ION Hotel Logo" className={styles.logo}/>
             <p>© ION-HOTEL 2024</p>
           </div>
-
-          {/* Navigation Links */}
           <div className={styles.footerSection}>
             <nav>
               <a href="#">Бронирование</a>
@@ -21,8 +18,6 @@ const Footer = () => {
               <a href="#">Спецпредложения</a>
             </nav>
           </div>
-
-          {/* More Links */}
           <div className={styles.footerSection}>
             <nav>
               <a href="#">Иннополис</a>
@@ -31,8 +26,6 @@ const Footer = () => {
               <a href="#">Контакты</a>
             </nav>
           </div>
-
-          {/* Contact Information */}
           <div className={styles.footerSection}>
             <nav>
               <a href="tel:+74952341206" className="text-orange-extra">+7 (495) 234-12-06</a>
@@ -41,8 +34,6 @@ const Footer = () => {
               <a href="#">Политика конфиденциальности</a>
             </nav>
           </div>
-
-          {/* Social Links */}
           <div className={styles.socialLinks}>
             <a href="#" className={styles.socialLink}>
               <Image src='/Icons_footer/telegram.svg' alt='telegram' width={20} height={20}/>
@@ -54,7 +45,7 @@ const Footer = () => {
               <Image src='/Icons_footer/youtube.svg' alt='youtube' width={20} height={20}/>
             </a>
           </div>
-          </div>
+        </div>
       </footer>
   );
 };
--- src/components/Layout/Mobile/FooterMobile.tsx
+++ src/components/Layout/Mobile/FooterMobile.tsx
@@ -1,12 +1,12 @@
-import React, { FC } from 'react';
+import React, {FC} from 'react';
 import styles from '@/styles/Mobile/Layout/FooterMobile.module.scss';
 import Image from "next/image";
 
 const FooterMobile: FC = () => {
   return (
       <div className={styles.footer}>
-        <Image src="/Logo/Logo_light.svg" alt="ION Hotel" width={100} height={40} />
-        <Image className={styles.line} src="/line.svg" alt='line' width={311} height={1}/>
+        <Image src="/Logo/Logo_light.svg" alt="ION Hotel" width={100} height={40}/>
+        <div className={styles.line}/>
         <div className={styles.contact}>
           <a href="tel:+74952341206" className="text-orange-extra">+7 (495) 234-12-06</a>
           <a href="tel:+74952343332" className="text-orange-extra">+7 (495) 234-33-32</a>
@@ -14,7 +14,7 @@ const FooterMobile: FC = () => {
         </div>
         <div className={styles.social}>
           <a href="#">
-          <Image src='/Icons_footer/telegram.svg' alt='telegram' width={32} height={32}/>
+            <Image src='/Icons_footer/telegram.svg' alt='telegram' width={32} height={32}/>
           </a>
           <a href="#">
             <Image src='/Icons_footer/vk.svg' alt='vk' width={32} height={32}/>
